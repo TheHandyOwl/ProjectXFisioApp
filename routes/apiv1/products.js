82b654426491e892a66ceecd2a24602f44de9a71
Carlos <carlos.companyperez@gmail.com>
2018-03-10 22:44:48 +0100

    Services and products updated successfully

--- routes/apiv1/products.js
+++ routes/apiv1/products.js
@@ -93,9 +93,13 @@ Router.delete('/:idProduct', function (req, res, next) {
 
 // Update a product
 
-Router.put('/:idProduct', function (req, res, next) {
+Router.put('/:id', function (req, res, next) {
 
-  Product.findOne({ idProduct: req.params.idProduct }, function (err, product) {
+  if ( (req.body.id != null) && (req.body.id != req.params.id) ) {
+    return res.status(422).json({ ok: false, message: res.__('product_information_error') });
+  }
+
+  Product.findOneAndUpdate({ _id: req.params.id, deleted: false }, req.body, function (err, product) {
     if (err) return next(err);
 
     if (!product) {
@@ -106,13 +110,7 @@ Router.put('/:idProduct', function (req, res, next) {
         }
       });
     } else if (product) {
-
-      Product.updateOne(req.body, function (err) {
-        if (err) return next(err);
-    
-        // product updated
-        return res.json({ ok: true, message: res.__('product_updated') });
-      });
+      return res.json({ ok: true, message: res.__('product_updated') });
     }
   });
 
