a2ccac4d3748c5bded7523f1075b534aee3dbabf
Carlos <carlos.companyperez@gmail.com>
2018-03-28 20:02:19 +0200

    Config files + gitignore

---
+++ config/config.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports.SEED = 'este-es-un-seed-dificil';
+
+// Google
+module.exports.GOOGLE_CLIENT_ID = '676307275703-l5vrbq1govljd304kkndd7j7i9b5vq0h.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'cipTGq9YHctJax57R1_nnKfz';
